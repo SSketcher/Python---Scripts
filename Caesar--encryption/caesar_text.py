67afcf0e4ca2e2e7bb6f57de9edbaa84fe94691d
Jakub Wojciechowski <jakubwojciechowski@users.noreply.github.com>
2020-06-10 18:05:58 +0200

    Caesar

--- Caesar--encryption/caesar_text.py
+++ Caesar--encryption/caesar_text.py
@@ -1,9 +1,5 @@
 import matplotlib.pyplot as plt
 
-with open ("C:/Users/menta/Desktop/STiOD Lab 1/tekst.txt", "r") as myfile:
-    text=myfile.read()
-
-
 def fromASCII(input_data):
     text = ""
     for n in input_data:
@@ -17,37 +13,81 @@ def toASCII(input_text):
         orginal_text.append(ord(char))
     return orginal_text
 
-def stringcomp(orginal_text,test_text):
-    if orginal_text == test_text:
-        return True
-    else: return False
-
-def encrypt(input_data,key_):
-    cipher = []
+def encrypt(input_data,s):
+    result = []
     for i in range(len(input_data)):
-        cipher.append((input_data[i] + s)%591)
-    return cipher
+        result.append((input_data[i] + s)%122)
+    return result
 
-def decrypt(input_data,key_):
-    cipher = []
+def decrypt(input_data,s):
+    result = []
     for i in range(len(input_data)):
-        cipher.append((input_data[i] - s)%591)
-    return cipher
+        result.append((input_data[i] - s)%122)
+    return result
+
+def makeHist(text, text_shifted):     #function for drawing histograms
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)      #setting subplot 
+    fig.suptitle('Comparison of the tonal distribution in the  images.', fontsize=16)       #setting figure title
+
+    axs[0].hist(text_shifted, bins = list(range(32,122)))       #making histogram and adding it to the subplot
+    axs[0].set_title('Histogram of shifted image')      #setting tittle of histogram
+    axs[0].set_xlabel('Colors')     # x axis name
+    axs[0].set_ylabel('Frequency')      # y axis name
 
 
+    axs[1].hist(toASCII(text), bins = list(range(32,122)))       #making histogram and adding it to the subplot
+    axs[1].set_title('Histogram of original image')
+    axs[1].set_xlabel('Colors')
+    axs[1].set_ylabel('Frequency')
 
-s = 20
+    plt.show()
 
+def main():     #main function
+    print('Do you want encrypt or decrypt text?')
+    flag = input('Enter enc or dec: ')     #entering the flag to set performed operation
+    if flag.lower() != 'enc' and flag.lower() != 'dec':       #checking if the flag was set correctly
+        raise ValueError("You entered wrong value")
 
+    path = input('Enter the file path: ')
+    if type(path) != str:       #path must be a string
+        raise TypeError("Path must be a string")
+    with open (path, "r") as myfile:
+        text = myfile.read()
 
-cipher = encrypt(toASCII(text),s)
-decipher = fromASCII(decrypt(cipher,s))
+    s = int(input('Enter value of shift: '))     #entering the shift valueenc
+    
+    if flag.lower() == 'enc':       #checking if the flag was set to encrypt
+        text_mod = encrypt(toASCII(text),s)       #encrypting text
+    elif flag.lower() == 'dec':       #checking if the flag was set to decrypt
+        text_mod = encrypt(toASCII(text),s)       #decrypting text
+    else:       #if the flag was set wrong
+        print('Something went wrong ... sorry')
 
-print("Encryption is corect: " + str(stringcomp(text,decipher)))
+    print('Do you whant to save result?')
+    sav = input('Enter Y or N: ')        #entering the flag for saving the result
+    if sav.upper() != 'Y' and sav.upper() != 'N':       #checking if the flag was set correctly
+        raise ValueError("You entered wrong value")
+    
+    if sav.upper() == 'Y':       #checking if the saving flag was set to Y
+        with open ('result_text.txt', "w") as myfile:        #saving result
+            myfile.write(fromASCII(text_mod))
+    if sav.upper() == 'N':       #checking if the saving flag was set to N
+        print(fromASCII(text_mod))      #displaying result
+    else:       #if the flag was set wrong
+        print('Something went wrong ... sorry')
 
+    print('Do you whant to show histogram')
+    his = input('Enter Y or N: ')        #entering the flag for making histogram
+    if his.upper() != 'Y' and his.upper() != 'N':       #checking if the flag was set correctly
+        raise ValueError("You entered wrong value")
+    
+    if his.upper() == 'Y':       #checking if the his was set to Y
+        makeHist(text,text_mod)      #using makeHist() function
+    elif his.upper() == 'N':       #checking if the his was set to N
+        pass      #doing nothing
+    else:       #if the flag was set wrong
+        print('Something went wrong ... sorry')
 
-plt.hist(cipher, bins = list(range(32,591)))
-plt.savefig('C:/Users/menta/Desktop/STiOD Lab 1/cesar_text_cipher_' + str(s) + '_hist.png')
-plt.clf()
-plt.hist(toASCII(text), bins = list(range(32,591)))
-plt.savefig('C:/Users/menta/Desktop/STiOD Lab 1/cesar_text_orginal_hist.png')
+    
+if __name__ == "__main__":
+    main()
