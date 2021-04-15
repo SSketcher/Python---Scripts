482373f68a604708a5c95e183165e9d3a5a262db
SSketcher <kubul10@op.pl>
2021-04-15 16:05:18 +0200

    Fixes and comments

--- Assembly_Line_Balancing/README.md
+++ Assembly_Line_Balancing/README.md
@@ -5,11 +5,19 @@ This folder contains assembly line balancing problem solver. A python script tha
 >An assembly line is a manufacturing process (often called a progressive assembly) in which parts (usually interchangeable parts) are added as the semi-finished assembly moves from workstation to workstation where the parts are added in sequence until the final assembly is produced. By mechanically moving the parts to the assembly work and moving the semi-finished assembly from work station to work station, a finished product can be assembled faster and with less labor than by having workers carry parts to a stationary piece for assembly.
 [Assembly line Wiki](https://en.wikipedia.org/wiki/Assembly_line)
 
-So by assembly line balancing problem, we mean finding a order of stations that gives as well optimized work flow. In order to solve the problem we use heuristics, to determin the rules that we use to order the stations.
+So by assembly line balancing problem, we mean finding a order of tasks, for given number of work stations, that gives as well optimized work flow. In order to solve the problem we use heuristics, to determin the rules that we use to order tasks.
 
 Suported heuristics:
-* WET
-* RPW
+* WET - Work Element Time
+* RPW - Ranked Positional Weight
+
+Terms and Paramiters:
+* K - Total number of workstations
+* C - Cycle time
+* LE - Line Efﬁciency
+* SE - Station time
+* T - Duration time
+
 
 ## Technologies
 * Python 3.7.3
@@ -21,3 +29,5 @@ Libraries:
 
 ## Sources and helpful materials
 [Mod-07 Lec-31 Line Balancing](https://www.youtube.com/watch?v=KA8jr4GLzhI)
+
+[A Multi-Objective Genetic Algorithm for Solving Assembly LineBalancing Problem](https://www.researchgate.net/publication/2830101_A_Multi-Objective_Genetic_Algorithm_for_Solving_Assembly_Line_Balancing_Problem)
--- Assembly_Line_Balancing/solver.py
+++ Assembly_Line_Balancing/solver.py
@@ -2,36 +2,36 @@ from operator import itemgetter
 import subprocess as sp
 import math  
 
-def dataInit(fileName):
+def dataInit(fileName):     #data initialization function
     tasks = []
-    with open (fileName, "r") as myfile:     #Reading text file
+    with open (fileName, "r") as myfile:     #Reading text file with graph
         data = myfile.readlines()
         for l in data:
-            temp = l.split()
-            if temp[0] == '1':
+            temp = l.split()        #spliting dthe text file by spaces
+            if temp[0] == '1':      
                 prev = None
             else:   
                 prev = int(temp[-1])
                 temp.pop(-1)
             temp.remove(';')
-            stage = {
-                'number' : int(temp[0]),
-                'time' : int(temp[1]),
-                'prev' : prev,
-                'nex' : list(int(n) for n in temp[2:]),
-                'per' : [0, 0]}
+            stage = {       #creating stage variable with all informations about single task
+                'number' : int(temp[0]),        #number of the task
+                'time' : int(temp[1]),          #necessary time to finish the task
+                'prev' : prev,                  #precedences tasks
+                'nex' : list(int(n) for n in temp[2:]),     #next task 
+                'per' : [0, 0]}                 #contains the time of start, and finish of the task. Before solving it's 0, 0
             tasks.append(stage)
     return tasks
 
 
-def grab(data, element):
+def grab(data, element):          #functions that returns element of given number 
     for task in data:
         if task['number'] == element:
             return task
     return None
 
 
-def folow(data, parm, task):
+def folow(data, parm, task):          #functions that follows the graph
     T = task[parm]
     next_task = task['nex']
     while len(next_task) == 1:
@@ -44,12 +44,12 @@ def folow(data, parm, task):
     return T
 
 
-def rpw(data):
+def rpw(data):          #ordering the task using WET heuristics
     order_rpw = []
     for task in data:
         T = folow(data, 'time', task)
         order_rpw.append([task['number'], T])
-    order_rpw = sorted(order_rpw, key = itemgetter(1), reverse = True)
+    order_rpw = sorted(order_rpw, key = itemgetter(1), reverse = True)  #sorting the order by the time of the task from the highest
     return order_rpw
 
 
@@ -65,7 +65,7 @@ def nextask(data, order):
     return temp[0]
 
 
-def wet(data):
+def wet(data):          #ordering the task using WET heuristics
     order = sorted(data, key = lambda i: i['time'], reverse = True)
     order_wet = []
     for item in order:
@@ -83,18 +83,17 @@ def wet(data):
     return order_wet
 
 
-def solve(data, order, k, offset):
+def solve(data, order, k, offset):          #main solving function solving the problem using given heuristic
     Et = int(sum(i['time'] for i in data))
     c = round(Et/k) + offset
     wT = c * k
     wLE = (Et/wT)*100
     line = []
     count = 1
-    for i in range(k):
+    for _ in range(k):
         flag = True
         stage = []
         ussed = []
-        T = i
         T = 0
         while flag:
             task = grab(data, order[0][0])
@@ -133,14 +132,14 @@ def solve(data, order, k, offset):
     return [line, order, c , coef]
 
 
-def solution(data):
+def solution(data):         #function diplaying the solution
     for i in range(len(data[0])):
         lin = '     ST' + str(i + 1) + '  '
         for item in data[0][i]:   
             lin += str(item['number']) + '(' + str(item['per'][1]) + ')  '
         print(lin)
     if len(data[1]) != 0:
-        unussed = 'Nie uszeregowane: '
+        unussed = 'Not lined up: '
         for i in data[1]:
             unussed += str(i[0]) + ' '
         print(unussed)
@@ -154,23 +153,28 @@ def solution(data):
     print("T = " + str(data[3][1]))
 
 
+def main():         #main function 
+    print('Enter the file path:')
+    fileName = input()
+    tasks = dataInit(fileName)
 
-print('Podaj nazwe pliku:')
-fileName = input()
-tasks = dataInit(fileName)
+    print('Enter the number of stations:')
+    k = int(input())
+    print('Enter estra time for each station:')
+    print('//0 - if none//')
+    offset = int(input())
 
-print('Podaj liczbę stacji:')
-k = int(input())
-print('Podaj dodatkowy czas dla każdej stacji:')
-print('//0 - jak bez dodatkowego//')
-offset = int(input())
+    sp.call('cls', shell=True)
 
-sp.call('cls',shell=True)
+    print()
+    RPW = solve(tasks, rpw(tasks), k, offset)
+    print('RPW,     c = ' + str(RPW[2]))
+    solution(RPW)
+    print('')
+    WET = solve(tasks, wet(tasks), k, offset)
+    print('WET,     c = ' + str(WET[2]))
+    solution(WET)
 
-RPW = solve(tasks, rpw(tasks), k, offset)
-print('RPW,     c = ' + str(RPW[2]))
-solution(RPW)
-print('')
-WET = solve(tasks, wet(tasks), k, offset)
-print('WET,     c = ' + str(WET[2]))
-solution(WET)
+
+if __name__ == "__main__":
+    main()
